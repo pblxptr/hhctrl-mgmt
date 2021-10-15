73310bb7b4da106f71803511625bbc1b05dc2a8e
bielu000 <bielu000@o2.pl>
2021-10-15 11:59:36 +0200

    Add optional build for curl and testing libraries

--- cmake/curl_ext_lib.cmake
+++ cmake/curl_ext_lib.cmake
@@ -1,22 +1,27 @@
-### PATHS ###
-set(CURL_DIR ${CMAKE_SOURCE_DIR}/lib/curl)
-set(CURL_INCLUDE_DIR ${CURL_DIR}/include)
-set(CURL_LIBRARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/curl/lib/.libs)
+find_package(CURL)
+
+if (NOT CURL_FOUND)
+  message("-- Adding ExternalProject_Add(curl)")
+  set(CURL_DIR ${CMAKE_SOURCE_DIR}/lib/curl)
+  set(CURL_INCLUDE_DIRS ${CURL_DIR}/include)
+  set(CURL_LIBRARIES ${CMAKE_CURRENT_BINARY_DIR}/curl/lib/.libs)
+
+  ExternalProject_Add(curl
+    GIT_REPOSITORY    "https://github.com/curl/curl.git"
+    GIT_TAG           master
+    SOURCE_DIR        ${CMAKE_SOURCE_DIR}/lib/curl
+    BINARY_DIR        ${CMAKE_CURRENT_BINARY_DIR}/curl
+    UPDATE_COMMAND    ""
+    CONFIGURE_COMMAND autoreconf -fi ${CURL_DIR}/configure.ac && ${CURL_DIR}/configure --disable-shared --enable-static --without-ssl --without-brotli --without-zlib
+    BUILD_COMMAND     make
+    INSTALL_COMMAND   ""
+    TEST_COMMAND      ""
+  )
+endif(NOT CURL_FOUND)
 
 ### LIB NAMES ###
 set(CURL_LIBRARY "-lcurl")
 
-ExternalProject_Add(curl
-  GIT_REPOSITORY    "https://github.com/curl/curl.git"
-  GIT_TAG           master
-  SOURCE_DIR        ${CMAKE_SOURCE_DIR}/lib/curl
-  BINARY_DIR        ${CMAKE_CURRENT_BINARY_DIR}/curl
-  UPDATE_COMMAND    ""
-  CONFIGURE_COMMAND autoreconf -fi ${CURL_DIR}/configure.ac && ${CURL_DIR}/configure --disable-shared --enable-static --without-ssl --without-brotli --without-zlib
-  BUILD_COMMAND     make # change to: make
-  INSTALL_COMMAND   ""
-  TEST_COMMAND      ""
-)
-include_directories(${CURL_INCLUDE_DIR})
-link_directories(${CURL_LIBRARY_DIR})
-
+### Add to paths ###
+include_directories(${CURL_INCLUDE_DIRS})
+link_directories(${CURL_LIBRARIES})
