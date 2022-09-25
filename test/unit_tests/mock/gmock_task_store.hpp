60eb7bbd14b9bc473381bfc2c01bc96ee3345fb1
Patryk Biel <patryk.biel.external@trumpf.com>
2022-09-25 20:37:30 +0200

    Add unit tests to CI

--- test/unit_tests/mock/gmock_task_store.hpp
+++ test/unit_tests/mock/gmock_task_store.hpp
@@ -1,16 +1,12 @@
-#include "../../../src/common/scheduler/include/scheduler/task_store.hpp"
+#include <scheduler/task_store.hpp>
 
-#include "../../../../../../.conan/data/gtest/cci.20210126/_/_/package/63868df56b76903d4ad40ecbd5b2e8238cee50c9/include/gmock/gmock.h"
+#include <gmock/gmock.h>
 
-namespace {
-using namespace common::scheduler;
-}
-
-class GMockTaskStore : public common::scheduler::TaskStore
+class GMockTaskStore : public common::scheduler::ITaskStore
 {
 public:
-  MOCK_METHOD(void, add, (TaskEntity), (override));
-  MOCK_METHOD(bool, exist, (const TaskEntity::Id_t&), (const override));
-  MOCK_METHOD(std::optional<TaskEntity>, find, (const TaskEntity::Id_t&), (const override));
-  MOCK_METHOD(void, remove, (const TaskEntity::Id_t&), (override));
+  MOCK_METHOD(void, add, (const common::scheduler::TaskEntity&), (override));
+  MOCK_METHOD(bool, exist, (const common::scheduler::TaskEntity::Id_t&), (const override));
+  MOCK_METHOD(std::optional<common::scheduler::TaskEntity>, find, (const common::scheduler::TaskEntity::Id_t&), (const override));
+  MOCK_METHOD(void, remove, (const common::scheduler::TaskEntity::Id_t&), (override));
 };
